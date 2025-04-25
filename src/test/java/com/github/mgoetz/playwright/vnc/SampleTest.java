c23843aab1dfb6ea9f372a250ae6096513e41a46
Maximilian Götz <maximilian.goetz@ars.de>
2025-04-25 20:53:12 +0200

    Start browser maximized.

--- src/test/java/com/github/mgoetz/playwright/vnc/SampleTest.java
+++ src/test/java/com/github/mgoetz/playwright/vnc/SampleTest.java
@@ -6,20 +6,25 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.Test;
 
-public class SampleTest {
+import java.util.List;
+
+class SampleTest {
 
     @Test
     void sampleTest() {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            Page page = browser.newPage();
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false)
+                    .setArgs(List.of("--start-maximized"))
+            );
+            Page page = browser.newContext(
+                    new Browser.NewContextOptions().setViewportSize(null)).newPage();
             for (int i = 0; i < 10; i++) {
                 page.navigate("https://playwright.dev/");
                 Thread.sleep(3000);
                 page.navigate("https://google.com/");
                 Thread.sleep(3000);
             }
-//            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
